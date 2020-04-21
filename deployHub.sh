37532fa7ec4d2f8ad5c4b771348f350bb8526ba1
petergreen86 <37148775+petergreen86@users.noreply.github.com>
2020-04-21 13:06:05 +0100

    update deployHub.sh
    changes made to deploy to an external azure postgresql database with medium hub sizing

--- deployHub.sh
+++ deployHub.sh
@@ -3,21 +3,64 @@
 # Author: Peter Green pgreen@synopsys.com
 # Description: Hub upgrade script
 # assumptions:
-# 1. swarm is already running
-# 2. you want snippet scanning enabled
+# 1. Swarm is already running
+# 2. You want snippet scanning enabled
 # 3. your secrets exist in /opt/secrets
 # 4. minimum alert version is 5.0.0 due to deployment differences in previous releases
+# 5. For external database access, HUB_POSTGRES_ADMIN_PASSWORD_FILE and HUB_POSTGRES_USER_PASSWORD_FILE must exist in /opt/secrets
+# 6. Azure DB SSL connections are enabled by default
+#
+# ***** PROVIDED AS IS WITH NO LIABILITY OR WARRANTY ******
+#
 ###################################################################
 
-HUB_RELEASE_VERSION=2019.8.1
-ALERT_RELEASE_VERSION=5.0.0
+HUB_RELEASE_VERSION=2020.2.1
+ALERT_RELEASE_VERSION=5.3.0
 DESTINATION_DIR="/opt"
+# DATABASE STUFF - CONFIGURE THIS
+DATABASE_HOST=pgreen-swarm.postgres.database.azure.com
+DATABASE_PORT=5432
+DATABASE_SSL=true
+DATABASE_USER=blackduck_user
+DATABASE_ADMIN=honadmin
+###################################
+#
+# DO NOT MODIFY THE BELOW VALUES
+#
+###################################
 WORKING_DIR="${DESTINATION_DIR}/hub-${HUB_RELEASE_VERSION}"
 ALERT_WORKING_DIR="${DESTINATION_DIR}/blackduck-alert-${ALERT_RELEASE_VERSION}-deployment"
 HUB_SOURCE="https://github.com/blackducksoftware/hub/archive/v${HUB_RELEASE_VERSION}.tar.gz"
 ALERT_SOURCE="https://github.com/blackducksoftware/blackduck-alert/releases/download/${ALERT_RELEASE_VERSION}/blackduck-alert-${ALERT_RELEASE_VERSION}-deployment.zip"
+HUB_POSTGRES_CONNECTION_USER=$DATABASE_USER@${HUB_POSTGRES_CONNECTION_USER:-$(awk -F "." '{print $1}' <<< $DATABASE_HOST)}
+HUB_POSTGRES_CONNECTION_ADMIN=$DATABASE_ADMIN@${HUB_POSTGRES_CONNECTION_ADMIN:-$(awk -F "." '{print $1}' <<< $DATABASE_HOST)}
+
 
 echo "Hub one step installer for $HUB_RELEASE_VERSION"
+
+checkWrite() {
+  if
+    [ -w $DESTINATION_DIR ]
+  then
+    echo "********************"
+    echo "directory is writable, ok to proceed with install"
+    echo "********************"
+  else
+    echo "********************"
+    echo "can't write to $DESTINATION_DIR, exiting!"
+    echo "********************"
+    exit 73
+  fi
+}
+
+checkHubExists() {
+  #checks whether the version you are trying to deploy already exists
+
+  if [ -d $WORKING_DIR ]; then
+    echo "$HUB_RELEASE_VERSION already exists - exiting!"
+    exit 78
+  fi
+}
 
 getHub() {
 
@@ -204,43 +247,6 @@ createSealSecret() {
 
 }
 
-# createSecrets() {
-#   # check if secrets files exist, if they do, create docker secret
-#   # if not, exit
-#   if [ ! \( -f "/opt/secrets/POSTGRES_USER_PASSWORD_FILE" -a -f "/opt/secrets/POSTGRES_ADMIN_PASSWORD_FILE" \) ]; then
-#     echo "One of the secrets does not exist - EXITING"
-#     exit -1
-#   else
-#     echo "secrets files found, let's create them"
-#     docker secret create hub_POSTGRES_USER_PASSWORD_FILE /opt/secrets/POSTGRES_USER_PASSWORD_FILE
-#     docker secret create hub_POSTGRES_ADMIN_PASSWORD_FILE /opt/secrets/POSTGRES_ADMIN_PASSWORD_FILE
-#   fi
-
-# }
-
-# pullExternalLocalOverrides() {
-#   # backup original local overrides
-#   if [ -f docker-compose.local-overrides.yml ]; then
-#     mv docker-compose.local-overrides.yml docker-compose.local-overrides.bak
-#   fi
-
-#   # pull custom local overrides with external db refs
-#   curlReturn=$(curl --silent -w "%{http_code}" -L -o "${WORKING_DIR}/docker-swarm/docker-compose.local-overrides.yml" "https://raw.githubusercontent.com/petergreen86/hub/master/docker-compose.local-overrides.yml")
-#   if [ 200 -eq $curlReturn ]; then
-#     echo "saved docker-compose.local-overrides.yml for external db"
-#   else
-#     echo "The curl response was ${curlReturn}, which is not successful - please check your configuration and environment."
-#     exit -1
-#   fi
-
-#   #update database variables
-#   echo "replacing database host and port"
-#   cd $WORKING_DIR/docker-swarm/
-#   sed -i "s/HUB_POSTGRES_HOST=/HUB_POSTGRES_HOST=$DATABASE_HOST/g" "hub-postgres.env"
-#   sed -i "s/HUB_POSTGRES_PORT=/HUB_POSTGRES_PORT=$DATABASE_PORT/g" "hub-postgres.env"
-
-# }
-
 createSimpleLocalOverrides() {
   #create a simple localoverrides with the seal key in it
   mv ${WORKING_DIR}/docker-swarm/docker-compose.local-overrides.yml ${WORKING_DIR}/docker-swarm/docker-compose.local-overrides.yml.bak
@@ -259,34 +265,126 @@ EOL
 
 }
 
-# deployHub-external() {
-#   echo "$PWD"
-#   docker stack deploy -c docker-compose.externaldb.yml -c docker-compose.local-overrides.yml hub
-# }
+createSimpleLocalOverridesAzure() {
+  #create a simple localoverrides with the seal key in it
+  mv ${WORKING_DIR}/docker-swarm/docker-compose.local-overrides.yml ${WORKING_DIR}/docker-swarm/docker-compose.local-overrides.yml.bak
+  #MEDIUM INSTANCE SETTINGS
+  cat >${WORKING_DIR}/docker-swarm/docker-compose.local-overrides.yml <<EOL
+version: '3.6'
+services:
+  webserver:
+    deploy:
+      resources:
+        limits: {memory: 2048M}
+        reservations: {memory: 2048M}
+  authentication:
+    secrets:
+      - HUB_POSTGRES_USER_PASSWORD_FILE
+      - HUB_POSTGRES_ADMIN_PASSWORD_FILE
+  webapp:
+    environment: {HUB_MAX_MEMORY: 4096m}
+    deploy:
+      resources:
+        limits: {cpus: '1', memory: 5120M}
+        reservations: {cpus: '1', memory: 5120M}
+    secrets:
+      - HUB_POSTGRES_USER_PASSWORD_FILE
+      - HUB_POSTGRES_ADMIN_PASSWORD_FILE
+  scan:
+    environment: {HUB_MAX_MEMORY: 4096m}
+    deploy:
+      replicas: 2
+      resources:
+        limits: {cpus: '1', memory: 5120M}
+        reservations: {cpus: '1', memory: 5120M}
+    secrets:
+      - HUB_POSTGRES_USER_PASSWORD_FILE
+      - HUB_POSTGRES_ADMIN_PASSWORD_FILE
+  jobrunner:
+    environment: {HUB_MAX_MEMORY: 6144m}
+    deploy:
+      replicas: 4
+      resources:
+        limits: {cpus: '1', memory: 7168M}
+        reservations: {cpus: '1', memory: 7168M}
+    secrets:
+      - HUB_POSTGRES_USER_PASSWORD_FILE
+      - HUB_POSTGRES_ADMIN_PASSWORD_FILE
+  uploadcache:
+    secrets:
+      - SEAL_KEY
+secrets:
+  SEAL_KEY:
+     external:
+       name: "hub_SEAL_KEY"
+  HUB_POSTGRES_USER_PASSWORD_FILE:
+     external:
+       name: "hub_HUB_POSTGRES_USER_PASSWORD_FILE"
+  HUB_POSTGRES_ADMIN_PASSWORD_FILE:
+     external:
+       name: "hub_HUB_POSTGRES_ADMIN_PASSWORD_FILE"
+       
+EOL
 
-checkWrite() {
-  if
-    [ -w $DESTINATION_DIR ]
-  then
+}
+
+  configureAzureDbCreds() {
+
+    echo "updating db creds..."
+    cd $WORKING_DIR/docker-swarm/
+    sed -i "/HUB_POSTGRES_HOST=/c\HUB_POSTGRES_HOST=$DATABASE_HOST" "hub-postgres.env"
+    sed -i "/HUB_POSTGRES_PORT=/c\HUB_POSTGRES_PORT=$DATABASE_PORT" "hub-postgres.env"
+    sed -i "/HUB_POSTGRES_USER=/c\HUB_POSTGRES_USER=$DATABASE_USER" "hub-postgres.env"
+    sed -i "/HUB_POSTGRES_ENABLE_SSL=false/c\HUB_POSTGRES_ENABLE_SSL=true" "hub-postgres.env"
+    sed -i "/HUB_POSTGRES_ADMIN=/c\HUB_POSTGRES_ADMIN=$DATABASE_ADMIN" "hub-postgres.env"
+    sed -i "/HUB_POSTGRES_ADMIN=/a HUB_POSTGRES_CONNECTION_USER=$HUB_POSTGRES_CONNECTION_USER" "hub-postgres.env"
+    sed -i "/HUB_POSTGRES_CONNECTION_USER=/a HUB_POSTGRES_CONNECTION_ADMIN=$HUB_POSTGRES_CONNECTION_ADMIN" "hub-postgres.env"
+  }
+
+  createPgUserSecret() {
+  DBUSERPW=$(docker secret inspect hub_HUB_POSTGRES_USER_PASSWORD_FILE | grep hub_HUB_POSTGRES_USER_PASSWORD_FILE | sed 's/[,"]//g' | awk '{print $2}')
+  if [ "${DBUSERPW}" = "hub_HUB_POSTGRES_USER_PASSWORD_FILE" ]; then
     echo "********************"
-    echo "directory is writable, ok to proceed with install"
-    echo "********************"
+    echo "HUB_POSTGRES_USER_PASSWORD_FILE exists"
   else
     echo "********************"
-    echo "can't write to $DESTINATION_DIR, exiting!"
+    echo "Creating HUB_POSTGRES_USER_PASSWORD_FILE secret"
     echo "********************"
-    exit 73
+    if [ ! -f ${DESTINATION_DIR}/secrets/HUB_POSTGRES_USER_PASSWORD_FILE ]; then
+      echo "********************"
+      echo "You must create HUB_POSTGRES_USER_PASSWORD_FILE key file in "${DESTINATION_DIR}/secrets" - exiting!"
+      echo "********************"
+      exit -1
+    else
+      docker secret create hub_HUB_POSTGRES_USER_PASSWORD_FILE ${DESTINATION_DIR}/secrets/HUB_POSTGRES_USER_PASSWORD_FILE
+    fi
   fi
-}
+  }
 
-checkHubExists() {
-  #checks whether the version you are trying to deploy already exists
-
-  if [ -d $WORKING_DIR ]; then
-    echo "$HUB_RELEASE_VERSION already exists - exiting!"
-    exit 78
+  createPgAdminSecret() {
+  DBADMINPW=$(docker secret inspect hub_HUB_POSTGRES_ADMIN_PASSWORD_FILE | grep hub_HUB_POSTGRES_ADMIN_PASSWORD_FILE | sed 's/[,"]//g' | awk '{print $2}')
+  if [ "${DBADMINPW}" = "hub_HUB_POSTGRES_ADMIN_PASSWORD_FILE" ]; then
+    echo "********************"
+    echo "HUB_POSTGRES_ADMIN_PASSWORD_FILE exists"
+  else
+    echo "********************"
+    echo "Creating HUB_POSTGRES_ADMIN_PASSWORD_FILE secret"
+    echo "********************"
+    if [ ! -f ${DESTINATION_DIR}/secrets/HUB_POSTGRES_ADMIN_PASSWORD_FILE ]; then
+      echo "********************"
+      echo "You must create HUB_POSTGRES_ADMIN_PASSWORD_FILE key file in "${DESTINATION_DIR}/secrets" - exiting!"
+      echo "********************"
+      exit -1
+    else
+      docker secret create hub_HUB_POSTGRES_ADMIN_PASSWORD_FILE ${DESTINATION_DIR}/secrets/HUB_POSTGRES_ADMIN_PASSWORD_FILE
+    fi
   fi
-}
+  }
+
+ deployHubExternal() {
+   echo "Starting Black Duck with Azure DB!"
+   docker stack deploy -c docker-compose.externaldb.yml -c docker-compose.local-overrides.yml hub
+ }
 
 cleanup() {
   echo "********************"
@@ -309,7 +407,7 @@ if [ $# -eq 0 ]; then
   exit 0
 fi
 
-while getopts "sah" OPTION; do
+while getopts "saeh" OPTION; do
   case "$OPTION" in
 
   s)
@@ -340,11 +438,27 @@ while getopts "sah" OPTION; do
     cleanup
     ;;
 
+  e)
+    #external - azure
+    checkHubExists
+    checkWrite
+    getHub
+    extractHub
+    createSealSecret
+    createPgUserSecret
+    createPgAdminSecret
+    createSimpleLocalOverridesAzure
+    configureAzureDbCreds
+    #databaseBackup
+    deployHubExternal
+    cleanup
+    ;;
   h)
     #help
     echo "usage:"
     echo "hubDeploy.sh -s deploys hub with internal postgres db"
     echo "hubDeploy.sh -a deploys hub with internal postgres db and alert"
+    echo "hubDeploy.sh -e deploys hub with Azure RDS with SSL"
     echo "hubDeploy.sh -h displays help output"
     exit 0
     ;;
